f2c086d9167ea1f26ef3d63611922d750163ef0f
sayed <sayedelgawy@gmail.com>
2022-01-07 19:58:57 +0200

    adding test function  to main

--- Source.cpp
+++ Source.cpp
@@ -11,47 +11,100 @@ using namespace std;
 
 int main()
 {
-	
 
+	////////////////////////////////Client////////////////////////////////////////////
+	
 	Client c;
-	c.instanceClient("sayed","1234",2000);//instance client with unique Id
+	c.instanceClient("sayed","1234",2000);//instance Client with unique Id
 	
 
 	FileManager f;
-	f.addClient(c);//add that client to database (Client.txt)
+	f.addClient(c);//add that Client to database (Client.txt)
 	
 
 	if (f.getAllClients().size() > 0)
 	{
-		f.getAllClients().back().displayInfo();//Display the last client info after loading data from database(Client.txt)
+		f.getAllClients().back().displayInfo();//Display the last Client info after loading data from database(Client.txt)
 	}
 	else {
 
 		cout << "no Data yet";
 	}
+
+
+	////////////////////////////////////Employee////////////////////////////////////////
+	/*
+	Employee c;
+	c.instanceEmployee("sayed","12345678",5000);//instance Employee with unique Id
+	
+
+	FileManager f;
+	f.addEmployee(c);//add that Employee to database (Employee.txt)
+	
+
+	if (f.getAllEmployee().size() > 0)
+	{
+		f.getAllEmployee().back().displayInfo();//Display the last Employee info after loading data from database(Employee.txt)
+	}
+	else {
+
+		cout << "no Data yet";
+	}
+
+	*/
+	
+	////////////////////////////////////Admin////////////////////////////////////////
+	/*
+	Admin c;
+	c.instanceAdmin("Ahmed","12345678",7000);//instance Admin with unique Id
+
+
+	FileManager f;
+	f.addAdmin(c);//add that Admin to database (Admin.txt)
+
+
+	if (f.getAllAdmins().size() > 0)
+	{
+		f.getAllAdmins().back()->displayInfo();//Display the last Admin info after loading data from database(Admin.txt)
+	}
+	else {
+
+		cout << "no Data yet";
+	}
+
+	*/
+
+	////////////////////////////////////Currency////////////////////////////////////////
+	/*
+	Currency c;
+	c.instanceCurrency("EGY-Pound",17.5);//instance Currency with unique Id
+
+
+	FileManager f;
+	f.addCurrency(c);//add that Currency to database (Currency.txt)
+
+
+	if (f.getAllCurrency().size() > 0)
+	{
+		f.getAllCurrency().back().displayInfo();//Display the last Admin info after loading data from database(Currency.txt)
+	}
+	else {
+
+		cout << "no Data yet";
+	}
+	*/
+
+	
+	
+
+
 	
 
 	
 	
 
 
-	
 
-	
-	
-
-
-
-	////Client c1("syaed", "1234", 5000);
-	////Client c2("ahmed", "1234", 5000);
-
-	//FileManager f1;
-	////f1.addClient(c1);
-	////f1.addClient(c2);
-
-	//
-
-	//cout << f1.test();
 
 
 
