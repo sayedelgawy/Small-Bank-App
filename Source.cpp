61e1aa33328130f25f18ed6bfe4409a429968dd0
sayed <sayedelgawy@gmail.com>
2022-02-12 09:23:49 +0200

    final touch adding info in source.cpp file

--- Source.cpp
+++ Source.cpp
@@ -1,127 +1,25 @@
 #include "StartUp.h"
 
 
-
-using namespace std;
-
-
-
 int main()
 {
+	/* You Can Try The Following Ids and Passwords:
+	
+		id: client_1041     pass: 1234
 
+		id: employee_2020   pass: 12345678
+
+		id: admin_3014      pass: 12345678
 	
 
-
-	StartUp::startProgram();
-	
-
-
-
-
-
-
-
-	
-	////////////////////////////////Client////////////////////////////////////////////
-	
-	//Client c;
-	//c.instanceClient("sayed","1234",2000);//instance Client with unique Id
-	//
-
-	//FileManager f;
-	//f.addClient(c);//add that Client to database (Client.txt)
-	//
-
-	//if (f.getAllClients().size() > 0)
-	//{
-	//	f.getAllClients().back().displayInfo();//Display the last Client info after loading data from database(Client.txt)
-	//}
-	//else {
-
-	//	cout << "no Data yet";
-	//}
-
-
-
-	////////////////////////////////////Employee////////////////////////////////////////
-	
-	//Employee c;
-	//c.instanceEmployee("sayed","12345678",5000);//instance Employee with unique Id
-	//
-
-	//FileManager f;
-	//f.addEmployee(c);//add that Employee to database (Employee.txt)
-	//
-
-	//if (f.getAllEmployee().size() > 0)
-	//{
-	//	f.getAllEmployee().back().displayInfo();//Display the last Employee info after loading data from database(Employee.txt)
-	//}
-	//else {
-
-	//	cout << "no Data yet";
-	//}
-
-	
-	
-	////////////////////////////////////Admin////////////////////////////////////////
-	
-	//Admin c;
-	//c.instanceAdmin("Ahmed","12345678",7000);//instance Admin with unique Id
-
-
-	//FileManager f;
-	//f.addAdmin(c);//add that Admin to database (Admin.txt)
-
-
-	//if (f.getAllAdmins().size() > 0)
-	//{
-	//	f.getAllAdmins().back()->displayInfo();//Display the last Admin info after loading data from database(Admin.txt)
-	//}
-	//else {
-
-	//	cout << "no Data yet";
-	//}
-
-	
-
-	////////////////////////////////////Currency////////////////////////////////////////
-	/*
-	Currency c;
-	c.instanceCurrency("EGY-Pound",17.5);//instance Currency with unique Id
-
-
-	FileManager f;
-	f.addCurrency(c);//add that Currency to database (Currency.txt)
-
-
-	if (f.getAllCurrency().size() > 0)
-	{
-		f.getAllCurrency().back().displayInfo();//Display the last Admin info after loading data from database(Currency.txt)
-	}
-	else {
-
-		cout << "no Data yet";
-	}
 	*/
+	StartUp::startProgram();
 
+
+
+	//Finally I Want to Say Thank You Eng(Mohamed Ali) Thank You eng(Ahmed Abdo) and Thank You eng(Basma)
 	
-	
-
-
-	
-
-	
-	
-
-
-
-
-
-
-	
-
-	
+	//Thank You Route
 
 	
 
